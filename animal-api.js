a05b99e64571509ae2b3dfd823d596558ddff4db
Michael Stallings <stall84@gmail.com>
2020-04-06 12:10:03 -0400

    think I've tempo'd solved the animal api exercise. got the get route returning json with the array of images in image folder.. now just have to randomize a single and format json output

--- animal-api.js
+++ animal-api.js
@@ -2,16 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-function dispImgs () {
-    fs.readdir('./public/images', (err,files) => {
-        if (err) {
-            console.log(`oopsie-woopsie, an error occurred: ${err}`);
-            return;
-        }
-        console.log(files);
-        
-    });
-};
+
 
 app.listen(3003, () => {
     console.log('Your Server is running on port 3003');
@@ -22,10 +13,23 @@ app.get('/', (req,res) => {
 });
 
 app.get('/files', (req,res) => {
-    dispImgs();
+    
     res.send('Attempting to get you a random dog image');
     console.log('Got a GET request at /files');
-})
+});
+
+app.get('/files/rando', (req,res,next) => {
+    next(fs.readdir('./public/images', (err,files) => {
+        if (err) {
+            console.log(`oopsie-woopsie, an error occurred: ${err}`);
+            return;
+        }
+        res.json({'files': files});
+        console.log(files);
+        
+    }));
+});
+ 
 
 app.use(express.static(__dirname + '/public'));
 
